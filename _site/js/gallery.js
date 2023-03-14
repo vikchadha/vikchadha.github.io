d1bc269237ef7c0204c6ca98c53351bf4c36965a
Vik Chadha <vikchadha@gmail.com>
2023-03-13 22:40:08 -0400

    Preload large images

--- _site/js/gallery.js
+++ _site/js/gallery.js
@@ -7,6 +7,11 @@ function activateGallery() {
     let mainImage  = document.querySelector("#gallery-photo > img");
     
     thumbnails.forEach(function(thumbnail) {
+      // Preload large images.
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      let largeVersion = new Image();
+      largeVersion.src = newImageSrc;
+
       thumbnail.addEventListener("click", function() {
         // Set clicked image as main image.
         let newImageSrc = thumbnail.dataset.largeVersion;
